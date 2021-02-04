7bd535187ebb4b584e932e5903942b7d4ff8408c
Hugo Ehlinger <hugoehlinger@laposte.net>
2021-02-04 15:12:36 +0100

    Final version of the cleaner code

---
+++ cleaner/src/helpers.py
@@ -0,0 +1,71 @@
+
+import nltk
+import json
+
+from wordcloud import WordCloud
+from collections import Counter
+
+import logging
+import logzero
+from logzero import logger
+
+logzero.loglevel(logging.WARNING)
+
+def character_transformer(document):
+    with_accent = ['é', 'è', 'à', "ê", "\u2019"]
+    without_accent = ['e', 'e', 'a', "e", "'"]
+    transformation_dict = {before:after for before, after in zip(with_accent, without_accent)}
+    return document.translate(str.maketrans(transformation_dict))
+
+
+def unicode_remover(document):
+    encoded = document.encode('ascii', 'replace')
+    return encoded.decode('utf-8')
+
+
+def character_remover(document):
+    characters_to_remove = ["@", "/", "#", ".", ",", "!", "?", 
+                            "(", ")", "-", "_","’","'", "\"", 
+                            ":", "\n", "\t", "\r"]
+    transformation_dict = {initial: " " for initial in characters_to_remove}
+    return document.translate(str.maketrans(transformation_dict))
+
+
+def contraction_transformer(document, filename):
+    with open(filename) as contractions:
+        for word in document.split():
+            if word in contractions:
+                document = document.replace(word, contractions[word])
+    return document
+
+
+def lemmatize(tokenized_document, stop_words, tag_dict):
+    lemmatizer = nltk.WordNetLemmatizer()
+    tokens_with_tags = nltk.pos_tag(tokenized_document)
+    lemmatized = []
+    for token, tag in tokens_with_tags:
+        if token not in stop_words:
+            convert_tag = tag_dict.get(tag[0], "n")
+            lemmatized.append(lemmatizer.lemmatize(token, convert_tag))
+    return lemmatized
+
+
+def save_wordcloud(df, idx, directory, mask):
+    filename = directory + str(idx) + "_word_cloud.png"
+    logger.warn(f' > WRITING {filename}')
+
+    df_mean = df.mean().sort_values(ascending=False).to_frame(name='tfidf mean')
+    dict_words_tfidf = df_mean[df_mean['tfidf mean'] != 0].to_dict()['tfidf mean']
+
+    wordcloud = WordCloud(height=600, width=800, background_color="white",
+        colormap='Blues', max_words=100, mask=mask,
+        contour_width=0.5, contour_color='lightsteelblue')
+    wordcloud.generate_from_frequencies(frequencies=dict_words_tfidf)
+    wordcloud.to_file(filename)     
+
+
+def save_tfidf(df, restaurant_id, directory, mask):
+    filename = directory + str(restaurant_id) + "_word_freq.csv"
+    logger.warn(f' > WRITING {filename}')
+
+    df.to_csv(filename)
---
+++ cleaner/src/main.py
@@ -0,0 +1,26 @@
+import argparse
+from cleaner import Cleaner
+from helpers import save_wordcloud, save_tfidf
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Cleaner and tokenizer of raw text stored as json file")
+    parser.add_argument('-f', '--files', nargs="*", type=str, help='path to the files to be cleaned')
+    parser.add_argument('-d', '--debug', help="prints intermediary logs", action="store_true")
+    parser.add_argument('-s', '--early_stop', type=int, default=-1, help='Caps the number of reviews to be processed')
+    args = parser.parse_args()
+
+    filenames = args.files
+
+    if args.early_stop == -1:
+        args.early_stop = None
+
+    cleaner = Cleaner(debug=args.debug, early_stop=args.early_stop)
+
+    for file in filenames:
+        cleaner.set_file(file)
+        cleaner.preprocessing(ngram=2)
+
+        cleaner.save_tokenized_corpus('./cleaned_data/')
+        cleaner.save_files('./cleaned_data/restaurant_wordclouds/', save_wordcloud, mask_path='assets/capgemini.jpg')
+        cleaner.save_files('./cleaned_data/restaurant_word_frequencies/', save_tfidf)
