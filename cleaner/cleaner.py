0bdd870100424244ca98fc1c927b62b10d4c0571
Hugo Ehlinger <hugoehlinger@laposte.net>
2021-02-04 12:58:08 +0100

    Refactoring

--- cleaner/cleaner.py
+++ cleaner/cleaner.py
@@ -7,21 +7,23 @@ import logging
 from logzero import logger
 import logzero
 
-from helpers import unicode_remover, punctuation_remover, character_transformer, contraction_transformer, lemmatize
+from helpers import unicode_remover, punctuation_remover, character_transformer, contraction_transformer, lemmatize, save_wordcloud, save_tfidf
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
 
 from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer
-from wordcloud import WordCloud
+
 from PIL import Image
 
 class Cleaner():
 
-    def __init__(self, stop_words_filename='custom_stop_words.txt', debug=0):
+    def __init__(self, stop_words_filename='custom_stop_words.txt', debug=False, early_stop=None):
+        
         self.init_stop_words(stop_words_filename)
         self.contraction_filename = 'contractions.json'
+        self.early_stop = early_stop
         self.tag_dict = {
             "J": wordnet.ADJ,
             "N": wordnet.NOUN,
@@ -31,7 +33,7 @@ class Cleaner():
 
         # Set logging level
         logzero.loglevel(logging.WARNING)
-        if int(debug) == 0 :
+        if debug is False :
             logging.disable(logging.WARNING)
 
 
@@ -46,7 +48,7 @@ class Cleaner():
         self.stop_words += lines
 
 
-    def set_file(self, filename, index_col='review_id', content_col='comment'):
+    def set_file(self, filepath, index_col='review_id', content_col='comment'):
         """ 
         Sets a new file to be cleaned:
             - self.tokenized_corpus: dict{int: review_id, list[str]: tokenized review (cleaned + split)}
@@ -60,9 +62,9 @@ class Cleaner():
             TypeError: if filename is not of type str
         """
 
-        if isinstance(filename, str):
-            self.filename = filename
-            json = pd.read_json(filename, lines=True)
+        if isinstance(filepath, str):
+            self.filename = filepath.split('/')[-1]
+            json = pd.read_json(filepath, lines=True)
             json.set_index(index_col, inplace = True)
             self.df = json
             self.index_col = index_col
@@ -78,8 +80,9 @@ class Cleaner():
 
         self.corpus = dict(zip(self.df.index, self.df[self.content_col]))
 
+
     def clean(self, document):
-        """ Cleans document (lower char, removes contractions, accents, punctuations, unicode) """
+        """ Cleans document (lower case + removes word contractions, accents, unicode char, and punctuation) """
     
         cleaned_document = document.lower()
         cleaned_document = contraction_transformer(cleaned_document, self.contraction_filename)
@@ -87,6 +90,7 @@ class Cleaner():
         cleaned_document = unicode_remover(cleaned_document)
         cleaned_document = punctuation_remover(cleaned_document)
         return cleaned_document
+
 
     def tokenize(self, document, ngram=1):
         """ 
@@ -107,8 +111,9 @@ class Cleaner():
         else:
             return tokenized_document, word_count
 
-    def preprocessing(self, ngram=1, early_stop=False):
-        """ Prepocesses corpus of documents (lower case + removes word contractions, accents, unicode char, and punctuation) """
+
+    def preprocessing(self, ngram=1):
+        """ Prepocesses corpus of documents by cleaning, tokenizing, and word count per document """
 
         logger.warn(f' > STARTING PREPROCESSING')
 
@@ -116,100 +121,71 @@ class Cleaner():
             raise ValueError("ngram argument must be strictly positive integer")
 
         for idx, review in self.corpus.items():
-
             if idx % 1000 == 0:
                 logger.warn(f' > CLEANING AND TOKENAZING REVIEW ({idx})')
 
             cleaned_review = self.clean(review)
+            
             if ngram > 1:
                 self.tokenized_corpus[idx], self.word_count[idx], self.tokenized_corpus_ngram[idx] = self.tokenize(cleaned_review, ngram)
             else:
                 self.tokenized_corpus[idx], self.word_count[idx] = self.tokenize(cleaned_review, ngram)
 
-            if early_stop and idx >= 100:
+            if self.early_stop is not None and idx >= self.early_stop:
+                logger.warn(f' > EARLY STOPPING AT IDX ({idx})')
                 break
-             
-    def preprocess_new_file(self, filename, index_col='review_id', content_col='comment', ngram=1):
-        """ Resets cleaner object attributes and preprocesses new file """
-        
-        self.set_file(filename, index_col, content_col)
-        self.preprocessing(ngram)
+
 
     def group_by_restaurant(self, restaurant_id):
-        """ Groups """
+        """ Sets tokenized corpus per restaurant and computes associated word count """
+        
         review_ids = self.df[self.df['restaurant_id'] == restaurant_id].index.values
         restaurant_counter = Counter()
-        restaurant_corpus, reviews_id_to_ret = [], []
+        restaurant_corpus, tokenized_reviews = [], []
+
         for review_id in review_ids:
             try:
                 restaurant_counter.update(self.word_count[review_id])
                 restaurant_corpus.append(" ".join(self.tokenized_corpus[review_id]))
-                reviews_id_to_ret.append(review_id)
+                tokenized_reviews.append(review_id)
             except:
                 pass
-        return restaurant_counter, restaurant_corpus, reviews_id_to_ret
+        return restaurant_counter, restaurant_corpus, tokenized_reviews
 
-    def get_word_count_by_restaurant(self, cols='restaurant_id'):
-        restaurant_list = [int(item) for item in self.df[cols].unique()]
+
+    def compute_restaurant_tfidf(self, col='restaurant_id'):
+        """ Computes TF-IDF Matrix of reviews per restaurant """
+
+        restaurant_list = [int(element) for element in self.df[col].unique()]
         
         for restaurant_idx in restaurant_list:
             try:
-                review_ids = self.df[self.df[cols] == restaurant_idx].index.values
-                review_ids = list(review_ids)
-                self.word_count_by_restaurant[restaurant_idx], self.tokenized_corpus_sentences[restaurant_idx], reviews_id_to_ret = self.group_by_restaurant(restaurant_idx)
-                
+                self.word_count_by_restaurant[restaurant_idx], self.tokenized_corpus_sentences[restaurant_idx], tokenized_reviews = self.group_by_restaurant(restaurant_idx)
                 vectorizer = TfidfVectorizer(stop_words='english')
                 vect_corpus = vectorizer.fit_transform(self.tokenized_corpus_sentences[restaurant_idx])
                 feature_names = np.array(vectorizer.get_feature_names())
-                self.df_word_frequency[restaurant_idx] = pd.DataFrame(data=vect_corpus.todense(), index=reviews_id_to_ret, columns=feature_names)
+                self.df_word_frequency[restaurant_idx] = pd.DataFrame(data=vect_corpus.todense(), index=tokenized_reviews, columns=feature_names)
             except:
-                pass 
+                pass
             
 
     def save_tokenized_corpus(self, directory):
         """ Saves tokenized corpus in json file """
-        with open(directory + 'tokenized' + self.filename, 'w') as tokenized_reviews:
-            logger.warn(f' > Writing tokenized_reviews.json')
+        
+        try:
+            os.mkdir(directory)
+        except OSError:
+            logger.warn("OSError: directory already exists")
+            
+        with open((directory + 'tokenized_' + self.filename), 'w') as tokenized_reviews:
+            logger.warn(f' > Writing tokenized_{self.filename}')
             json.dump(self.tokenized_corpus, tokenized_reviews)
 
-    def save_wordclouds(self, restaurant_ids='all', directory='./cleaned_data/restaurant_wordclouds/'):
 
-        def save_wordcloud(df_tfidf, restaurant_id, directory, capgemini_mask):
-            filename = directory + "restaurant_" + str(restaurant_id) + "_word_cloud.png"
-            df_tfidf_mean = df_tfidf.mean().sort_values(ascending=False).to_frame(name='tfidf mean')
-            dict_words_tfidf = df_tfidf_mean[df_tfidf_mean['tfidf mean'] != 0].to_dict()['tfidf mean']
-
-            wordcloud = WordCloud(height=600, width=800, background_color="white",
-                colormap='Blues', max_words=100, mask=capgemini_mask,
-                contour_width=0.5, contour_color='lightsteelblue')
-            wordcloud.generate_from_frequencies(frequencies=dict_words_tfidf)
-            wordcloud.to_file(filename)     
-
-            logger.warn(f' > WRITING {filename}')
-
-        capgemini_mask = np.array(Image.open("./capgemini.jpg"))
-
-        try:
-            os.mkdir(directory)
-        except OSError:
-            logger.warn("OSError: directory already exists")
-
-        if restaurant_ids == 'all':
-            for restaurant_id, df_tfidf in self.df_word_frequency.items():
-                save_wordcloud(df_tfidf, restaurant_id, directory, capgemini_mask)
-                
-        else:
-            for restaurant_id in restaurant_ids:
-                df_tfidf = self.df_word_frequency[restaurant_id]
-                save_wordcloud(df_tfidf, restaurant_id, directory, capgemini_mask)
-
-
-    def write_tfidfs(self, restaurant_ids='all', directory='./cleaned_data/restaurant_word_frequencies/'):
+    def save_files(self, directory, file_type, callable_name, mask_path, restaurant_ids='all'):
+        """ Saves files (Wordclouds or TF-IDF) for corpora """
         
-        def write_tfidf(df_tfidf, restaurant_id, directory):
-            filename = directory + "restaurant_" + str(restaurant_id) + "_word_freq.csv"
-            df_tfidf.to_csv(filename)
-            logger.warn(f' > WRITING {filename}')
+        logger.warn(f' > SAVING {file_type} FILES ')
 
         try:
             os.mkdir(directory)
@@ -217,10 +193,9 @@ class Cleaner():
             logger.warn("OSError: directory already exists")
 
         if restaurant_ids == 'all':
-            for restaurant_id, df_tfidf in self.df_word_frequency.items():
-                write_tfidf(df_tfidf, restaurant_id, directory)
+            for restaurant_id, df in self.df_word_frequency.items():
+                callable_name(df, self.filename, restaurant_id, directory, np.array(Image.open(mask_path)))
         else:
             for restaurant_id in restaurant_ids:
-                df_tfidf = self.df_word_frequency[restaurant_id]
-                write_tfidf(df_tfidf, restaurant_id, directory)
-                
+                df = self.df_word_frequency[restaurant_id]
+                callable_name(df, self.filename, restaurant_id, directory, np.array(Image.open(mask_path)))
--- cleaner/helpers.py
+++ cleaner/helpers.py
@@ -7,7 +7,9 @@ from logzero import logger
 import logging
 logzero.loglevel(logging.WARNING)
 
+from wordcloud import WordCloud
 from collections import Counter
+
 
 def character_transformer(document):
     with_accent = ['é', 'è', 'à', "ê", "\u2019"]
@@ -15,9 +17,11 @@ def character_transformer(document):
     transformation_dict = {before:after for before, after in zip(with_accent, without_accent)}
     return document.translate(str.maketrans(transformation_dict))
 
+
 def unicode_remover(document):
     encoded = document.encode('ascii', 'replace')
     return encoded.decode('utf-8')
+
 
 def punctuation_remover(document):
     # In most of the case punctuation do not help on understanding a sentence or a doc
@@ -27,12 +31,14 @@ def punctuation_remover(document):
     transformation_dict = {initial: " " for initial in characters_to_remove}
     return document.translate(str.maketrans(transformation_dict))
 
+
 def contraction_transformer(document, filename):
     with open(filename) as contractions:
         for word in document.split():
             if word in contractions:
                 document = document.replace(word, contractions[word])
     return document
+
 
 def lemmatize(tokenized_document, stop_words, tag_dict):
     lemmatizer = nltk.WordNetLemmatizer()
@@ -43,3 +49,24 @@ def lemmatize(tokenized_document, stop_words, tag_dict):
             convert_tag = tag_dict.get(tag[0], "n")
             lemmatized.append(lemmatizer.lemmatize(token, convert_tag))
     return lemmatized
+
+
+def save_wordcloud(df, filename, idx, directory, mask):
+
+    filename = directory + filename + str(idx) + "_word_cloud.png"
+    df_mean = df.mean().sort_values(ascending=False).to_frame(name='tfidf mean')
+    dict_words_tfidf = df_mean[df_mean['tfidf mean'] != 0].to_dict()['tfidf mean']
+
+    wordcloud = WordCloud(height=600, width=800, background_color="white",
+        colormap='Blues', max_words=100, mask=mask,
+        contour_width=0.5, contour_color='lightsteelblue')
+    wordcloud.generate_from_frequencies(frequencies=dict_words_tfidf)
+    wordcloud.to_file(filename)     
+
+    logger.warn(f' > WRITING {filename}')
+
+
+def save_tfidf(df, filename, restaurant_id, directory, mask):
+    filename = directory + filename + str(restaurant_id) + "_word_freq.csv"
+    df.to_csv(filename)
+    logger.warn(f' > WRITING {filename}')
