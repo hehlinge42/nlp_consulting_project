7c8bde8fb01c9d1ca01b8f0d7797562e3315a663
Hugo Ehlinger <hugoehlinger@laposte.net>
2021-02-20 13:54:59 +0100

    Added merge_restaurants.py

--- ignore_large_files.sh
+++ ignore_large_files.sh
@@ -4,4 +4,4 @@ sed '/# Large files/,$d' < .gitignore > tmp
 rm .gitignore
 mv tmp .gitignore
 echo '# Large files' >> .gitignore
-find . -not -path '*/\.*' -size +85M | cut -c 3- >> .gitignore
+find . -not -path '*/\.*' -size +95M | cut -c 3- >> .gitignore
--- scraper/scraper_rooftops/merge_restaurants.py
+++ scraper/scraper_rooftops/merge_restaurants.py
@@ -30,5 +30,20 @@ if not os.path.exists(merged_data_dir):
     logger.warn(f"Creating directory {merged_data_dir}")
     os.makedirs(merged_data_dir)
 
+merged_file = os.path.join(merged_data_dir, "merged_restaurants.json")
+
 logger.info("Merging restaurant files")
-merge_files(os.path.join(scraped_data_dir, 'restaurants'), os.path.join(merged_data_dir, "merged_restaurants.json"))
+merge_files(os.path.join(scraped_data_dir, 'restaurants'), merged_file)
+
+already_scraped_list = [10216, 13639, 12755, 14787, 14657, 10068, 13460, 13836, 12125, 9796, 11264]
+all_restaurant_path = os.path.join(os.getcwd(), 'scraper', 'scraper_restaurants', 'scraped_data', 'restaurants', 'restaurants_run_1.json')
+all_restaurants = pd.read_json(all_restaurant_path, lines=True)
+all_restaurants.set_index(['restaurant_id'], inplace=True)
+rooftops = all_restaurants.loc[already_scraped_list, :]
+
+logger.info(f"Concatenating with {len(rooftops)} already scraped restaurants")
+
+merged_rooftops = pd.read_json(merged_file, lines=True)
+merged_rooftops.set_index(['restaurant_id'], inplace=True)
+merged_rooftops = pd.concat([merged_rooftops, rooftops])
+merged_rooftops.to_csv(os.path.join(os.getcwd(), 'scraper', 'scraped_data', 'merged_data', 'merged_rooftops.csv'))
