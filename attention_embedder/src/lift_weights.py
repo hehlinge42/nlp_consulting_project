03f84aa747bacd4af13bfbe4fe926d251ca4d7f5
Hugo Ehlinger <hugoehlinger@laposte.net>
2021-03-05 20:28:11 +0100

    fkglhkj

--- attention_embedder/src/lift_weights.py
+++ attention_embedder/src/lift_weights.py
@@ -1,11 +1,12 @@
 import json
 import os
 
-with open('../data/weights_gz.json', 'r') as weights_file:
+with open(os.path.join('..', 'data', 'weights_json.json'), 'r') as weights_file:
     weights = json.load(weights_file)
     ret = {}
     ret[1] = weights['0']
     ret[5] = weights['4']
     ret[10] = weights['9']
     ret[20] = weights['19']
-    json.dump(ret, open(os.path.join("..", "data", "light_weights_gz.json"),"w+"))
+    print(len(ret))
+    json.dump(ret, open(os.path.join("..", "data", "light_weights_json.json"),"w+"))
